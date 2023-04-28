5d19da9854f8605c4b59458064c2dd41b5718594
GutemaG <bir13gud@gmail.com>
2023-04-28 21:44:17 +0300

    add proxima nova font

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -3,6 +3,6 @@ class Cost < ApplicationRecord
   has_many :group_costs, dependent: :destroy
   has_many :groups, through: :group_costs, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: { scope: :author, message: 'You already have an expense with this item' }
+  validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
